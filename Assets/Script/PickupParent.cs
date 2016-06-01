25c5d495027db4355cfdde3c2fcfde6b4f30d431
hsuster <andrewhsu42@gmail.com>
2016-05-31 21:40:44 -0700

    Added Input Events for controller

---
+++ Assets/Script/PickupParent.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class PickupParent : MonoBehaviour {
+
+    SteamVR_TrackedObject trackedObj;
+    SteamVR_Controller.Device device;
+
+    void Awake () {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+	
+    // Called for every physics step (fixed interval between calls)
+	void FixedUpdate () {
+        SteamVR_Controller.Device device = SteamVR_Controller.Input((int)trackedObj.index);
+        if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You are holding 'Touch' on the Trigger");
+        }
+
+        if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You have activated 'Touch Down' on the Trigger");
+        }
+
+        if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You have activated 'Touch Up' on the Trigger");
+        }
+
+        if (device.GetPress(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You are holding 'Press' on the Trigger");
+        }
+
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You have activated 'Press Down' on the Trigger");
+        }
+
+        if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You have activated 'Press Up' on the Trigger");
+        }
+    }
+}
